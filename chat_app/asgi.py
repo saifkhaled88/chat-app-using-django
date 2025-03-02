7ebf39abe1f1cb3a26562b76a3957a1feec666a6
saifkhaled88 <93052650+saifkhaled88@users.noreply.github.com>
2025-03-02 20:29:56 +0200

    Update asgi.py

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+# AuthMiddlewareStack to attach user authentication to WebSocket requests.
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
